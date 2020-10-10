b99e1ebae158778fb9c4a6d6ad45773ed12723d7
Rodrigo Escobar <ocralo@gmail.com>
2020-10-09 22:42:27 -0500

    Add files via upload

---
+++ precache-manifest.a99182bd6bad230e55e759b3b6337359.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6a4550e1e05c5c2266129634f1ccf0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "83c2bbebcfd44a24504d",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a43e690057f2532354cf",
+    "url": "./static/css/main.4a67e986.chunk.css"
+  },
+  {
+    "revision": "83c2bbebcfd44a24504d",
+    "url": "./static/js/2.623b9cb1.chunk.js"
+  },
+  {
+    "revision": "b6840d1fa4cd3c3e88b7c215b28b2e9e",
+    "url": "./static/js/2.623b9cb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a43e690057f2532354cf",
+    "url": "./static/js/main.133efbb0.chunk.js"
+  },
+  {
+    "revision": "3e7d55308fc8bdf4fcdf",
+    "url": "./static/js/runtime-main.b257aece.js"
+  },
+  {
+    "revision": "a70da5d019337023de7cd453ce59014b",
+    "url": "./static/media/eccomerce-360.a70da5d0.svg"
+  },
+  {
+    "revision": "1d18a06643b4589f73ae534a2d81b043",
+    "url": "./static/media/eccomerce-web.1d18a066.svg"
+  },
+  {
+    "revision": "ced558eaa277b4e1e0ede3d86d872b80",
+    "url": "./static/media/hoyostools.ced558ea.jpeg"
+  }
+]);
